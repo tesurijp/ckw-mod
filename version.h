a50d6f8720a4414b1b580725521234843ab8378b
RuRuRu <rururutan+github@gmail.com>
2016-01-08 23:09:11 +0900

    Happy new year.

--- version.h
+++ version.h
@@ -4,5 +4,5 @@
 #define VERSION_STRING "0.9.1"
 #define VERSION_NUMSTRING "00901"
 #define BUILDDATE_STRING "2013/03/10  1:47:55 UTC+9"
-#define COPYRIGHT_STRING "Copyright (C) 2005-2010  Kazuo Ishii and ckw-mod creators and more..."
-#define ABOUT_DLG_STRING "ckw-mod version 0.9.1 , build 2013/03/10  1:47:55 UTC+9\r\nCopyright (C) 2005-2010  Kazuo Ishii and ckw-moders"
+#define COPYRIGHT_STRING "Copyright (C) 2005-2016  Kazuo Ishii and ckw-mod creators and more..."
+#define ABOUT_DLG_STRING "ckw-mod version 0.9.1 , build 2013/03/10  1:47:55 UTC+9\r\nCopyright (C) 2005-2016  Kazuo Ishii and ckw-moders"
