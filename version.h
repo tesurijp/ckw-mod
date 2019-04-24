986c548c38d457784fb32b8dbe4af9c93266765d
tesuri <tesuri@gmail.com>
2019-04-24 23:27:54 +0900

    バージョン情報変更

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 9
 #define VERSION_BUGFIX 1
-#define VERSION_STRING L"0.9.1"
+#define VERSION_STRING L"0.9.1-tesuri"
 #define VERSION_NUMSTRING L"00901"
-#define BUILDDATE_STRING L"2016/01/21 22:08:14 UTC+9"
-#define COPYRIGHT_STRING L"Copyright (C) 2005-2016  Kazuo Ishii and ckw-mod creators and more..."
-#define ABOUT_DLG_STRING L"ckw-mod version 0.9.1 , build 2016/01/21 22:08:14 UTC+9\r\nCopyright (C) 2005-2016  Kazuo Ishii and ckw-moders"
+#define BUILDDATE_STRING L"2019/04/24 23:24:28 UTC+9"
+#define COPYRIGHT_STRING L"Copyright (C) 2005-2019 tesuri, Kazuo Ishii and ckw-mod creators and more..."
+#define ABOUT_DLG_STRING L"ckw-mod version 0.9.1 , build 2019/04/24 23:24:28 UTC+9\r\nCopyright (C) 2005-2016  Kazuo Ishii and ckw-moders"
