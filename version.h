964a987dadebdf8f74f9bcc5d9122d86bc33c793
tesuri <tesuri@gmail.com>
2019-04-24 23:35:54 +0900

    x64ビルドの追加

--- version.h
+++ version.h
@@ -3,6 +3,6 @@
 #define VERSION_BUGFIX 1
 #define VERSION_STRING L"0.9.1"
 #define VERSION_NUMSTRING L"00901"
-#define BUILDDATE_STRING L"2019/04/24 23:29:52 UTC+9"
+#define BUILDDATE_STRING L"2019/04/24 23:33:55 UTC+9"
 #define COPYRIGHT_STRING L"Copyright (C) 2005-2017  Kazuo Ishii and ckw-mod creators and more..."
-#define ABOUT_DLG_STRING L"ckw-mod version 0.9.1 , build 2019/04/24 23:29:52 UTC+9\r\nCopyright (C) 2005-2016  Kazuo Ishii and ckw-moders"
+#define ABOUT_DLG_STRING L"ckw-mod version 0.9.1 , build 2019/04/24 23:33:55 UTC+9\r\nCopyright (C) 2005-2016  Kazuo Ishii and ckw-moders"
